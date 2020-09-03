11865a84010861079bf47d5cf0659dce55a333f4
ahmedeid <kingmedo954@gmail.com>
2020-09-03 16:22:42 +0200

    create models[product, shopping_cart, shopping_cart_item] & view[index: show list of all items]

--- Rest_Api/urls.py
+++ Rest_Api/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+import store.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', store.views.index, name="list-products"),
+    path('cart', store.views.cart, name="shopping-cart"),
+    path('products/<int:id>', store.views.show, name='show-product'),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from store.models import Product, ShoppingCart
+
 
 # Create your views here.
+def index(request):
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'store/product_list.html', context)
+
+
+def cart(request):
+    return None
+
+
+def show(request):
+    return None
