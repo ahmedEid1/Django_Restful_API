4d57938eef10c636b05f3a63ce06bf33202f5d9d
ahmedeid <kingmedo954@gmail.com>
2020-09-04 18:06:47 +0200

    adding filter[id, on_sale] & search

--- store/api_views.py
+++ store/api_views.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from django.utils import timezone
+from  rest_framework.filters import SearchFilter
 
 from store.serializers import ProductSerializer
 from store.models import Product
@@ -7,4 +10,22 @@ from store.models import Product
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filter_fields = ('id',)
+    search_fields = ('name', 'description')
 
+    def get_queryset(self):
+        on_sale = self.request.query_params.get('on_sale', None)
+
+        if on_sale is None:
+            return super().get_queryset()
+
+        queryset = Product.objects.all()
+        if on_sale.lower() == "true":
+            now = timezone.now()
+            return queryset.filter(
+                sale_start__lte=now,
+                sale_end__gte=now
+            )
+
+        return queryset
