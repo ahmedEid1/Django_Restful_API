66442cc5ccde9c0bdb96f2380a90202e99526b38
ahmedeid <kingmedo954@gmail.com>
2020-09-04 17:49:51 +0200

    serializing the Product Model & creating a view and a url for the products list

--- Rest_Api/urls.py
+++ Rest_Api/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 import store.views
+import store.api_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +27,6 @@ urlpatterns = [
     path('cart', store.views.cart, name="shopping-cart"),
     path('products/<int:id>', store.views.show, name='show-product'),
 
+    path('api/products', store.api_views.ProductList.as_view()),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
