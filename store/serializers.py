3b8f299e33fce4273c98303d7452cee782add777
ahmedeid <kingmedo954@gmail.com>
2020-09-05 14:43:35 +0200

    adding Photo and file fields to the serializer

--- store/serializers.py
+++ store/serializers.py
@@ -27,15 +27,25 @@ class ProductSerializer(serializers.ModelSerializer):
         allow_null=True, help_text="allowed format is: 12:05 PM 16 April 2019",
         style={'input_type': 'text', 'placeholder': '12:05 PM 16 April 2019'}
     )
+    photo = serializers.ImageField(default=None)
+    warranty = serializers.FileField(write_only=True, default=None)
 
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'sale_start', 'sale_end',
-                  'is_on_sale', 'current_price', 'cart_items')
+                  'is_on_sale', 'current_price', 'cart_items', 'photo', 'warranty')
 
     def get_cart_items(self, instance):
         items = ShoppingCartItem.objects.filter(product=instance)
         return CartItemSerializer(items, many=True).data
+
+    def update(self, instance, validated_data):
+        if validated_data.get('warranty', None):
+            instance.description += '\n\nWarranty:\n'
+            instance.description += b'; '.join(
+                validated_data['warranty'].readlines()
+            ).decode()
+        return instance
 
 
 class ProductStateSerializer(serializers.Serializer):
